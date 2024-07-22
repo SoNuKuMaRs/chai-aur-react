ef30425a925fb00a50a26033f035fa42d4398931
SoNuKuMaRsr <sonukumarsingh1619@gmail.com>
2024-07-22 13:51:02 +0530

    tailwildprops

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  const [count, setCount] = useState(0);
+  let myObj = {
+    username: "sonu",
+    age: 22
+  }
+
+  let newArr = [1,2,3]
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind test</h1>
+      <Card username = "chai aur react" btnText ="Click here" />
+      <Card username="sonu kumar" btnText ="Visit Me" />
+
+
+     
+    </>
+  );
+}
+
+export default App;
